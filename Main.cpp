93e8cbedd8b22de0d0b7d9d671a787d7b55b8020
Kamalpreet Mundi <kamalmundi31@gmail.com>
2022-03-30 16:43:19 -0600

    Ground Plane Rendered. Testing Shadow mapping and other things

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+
 //------- Ignore this ----------
 #include<filesystem>
 //------------------------------
@@ -17,6 +18,8 @@
 #include"EBO.h"
 #include"Camera.h"
 #include <vector>
+
+void renderQuad();
 
 
 
@@ -97,6 +100,10 @@ int main()
 
 	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
+	Shader groundShader("ground.vert", "ground.frag");
+	// Shadow Mapping Shaders
+	Shader simpleDepthShader("shadow.vert", "shadow.frag");
+	Shader debugDepthShader("shadowDebug.vert", "shadowDebug.frag");
 
 
 
@@ -111,6 +118,7 @@ int main()
 	}
 
 	//fill plane indices array
+	
 	GLuint* id = &indices[0];
 	for (i = 0; i < NUM_Z; i++) {
 		for (j = 0; j < NUM_X; j++) {
@@ -118,6 +126,7 @@ int main()
 			int i1 = i0 + 1;
 			int i2 = i0 + (NUM_X + 1);
 			int i3 = i2 + 1;
+			//std::cout << "Val: " << i0 + " " << i1 << " " << i2 << " " << i3 << "\n";
 			if ((j + i) % 2) {
 				*id++ = i0; *id++ = i2; *id++ = i1;
 				*id++ = i1; *id++ = i2; *id++ = i3;
@@ -128,9 +137,12 @@ int main()
 			}
 		}
 	}
+	
 	std::vector<GLfloat> verts;
 	for (j = 0; j <= NUM_Z; j++) {
 		for (i = 0; i <= NUM_X; i++) {
+			//std::cout << "x: " << ((float(i) / (NUM_X - 1)) * 2 - 1) * HALF_SIZE_X << "\n";
+			//std::cout << "z: " << ((float(j) / (NUM_Z - 1)) * 2 - 1) * HALF_SIZE_Z << "\n";
 			verts.push_back(((float(i) / (NUM_X - 1)) * 2 - 1) * HALF_SIZE_X);
 			verts.push_back(0);
 			verts.push_back(((float(j) / (NUM_Z - 1)) * 2 - 1) * HALF_SIZE_Z);
@@ -163,10 +175,44 @@ int main()
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	float vertGround[] = {
+		// positions          // colors           // texture coords
+			  2.0f,  -1.5f, 2.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+			  2.0f, -1.5f, -2.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+			 -2.0f, -1.5f, -2.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+			 -2.0f,  -1.5f, 2.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	unsigned int indexGround[] = {
+	   0, 1, 3, // first triangle
+	   1, 2, 3  // second triangle
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertGround), vertGround, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexGround), indexGround, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	// Original code from the tutorial
 	Texture brickTex("textures/brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	brickTex.texUnit(shaderProgram, "tex0", 0);
+	brickTex.texUnit(groundShader, "tex0", 0);
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -176,12 +222,40 @@ int main()
 	// Creates camera object
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
+	// Shadow Mapping Stuff
+	unsigned int depthMapFBO;
+	glGenFramebuffers(1, &depthMapFBO);
+
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+	// Depth map for shadowMappings
+	unsigned int depthMap;
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+		SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+
 
 
 
 	const float amplitude = 0.125;
 	const float frequency = 4;
 	const float PI = 3.14159;
+
+	debugDepthShader.Activate();
+	glUniform1i(glGetUniformLocation(debugDepthShader.ID, "DepthMap"), 0);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -195,16 +269,58 @@ int main()
 		//	i += 8;
 		//}
 
-
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Shadow mapping Stuff
+		/*
+		float near_plane = 1.0f, far_plane = 10.5f;
+		glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+		//glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, near_plane, far_plane);
+		glm::mat4 lightView = glm::lookAt(glm::vec3(0.0f,4.0f, 0.1f),
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+
+		simpleDepthShader.Activate();
+		glUniformMatrix4fv(glGetUniformLocation(simpleDepthShader.ID, "lightSpaceMatrix"),1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+
+		
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+			glClear(GL_DEPTH_BUFFER_BIT);
+			glActiveTexture(GL_TEXTURE0);
+			brickTex.Bind();
+			glm::mat4 model = glm::mat4(1.0f);
+			glUniformMatrix4fv(glGetUniformLocation(simpleDepthShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		debugDepthShader.Activate();
+		glUniform1f(glGetUniformLocation(debugDepthShader.ID, "near_plane"), near_plane);
+		glUniform1f(glGetUniformLocation(debugDepthShader.ID, "far_plane"), far_plane);
+		//debugDepthShader.setFloat("near_plane", near_plane);
+		//debugDepthQuad.setFloat("far_plane", far_plane);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		renderQuad();
+		*/
+
+
+		
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
-
+		//std::cout << glfwGetTime() << "\n";
 		glUniform1f(glGetUniformLocation(shaderProgram.ID, "time"), glfwGetTime());
+		//glUniform1f(glGetUniformLocation(shaderProgram.ID, "time"), 1.0f);
 
 		// Handles camera inputs
 		camera.Inputs(window);
@@ -218,6 +334,23 @@ int main()
 		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		groundShader.Activate();
+		brickTex.Bind();
+		glBindVertexArray(VAO);
+		camera.Matrix(45.0f, 0.1f, 100.0f, groundShader, "camMatrix");
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//simpleDepthShader.use();
+		//simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+		
+
+
+
+
+
+
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
@@ -237,3 +370,39 @@ int main()
 	glfwTerminate();
 	return 0;
 }
+
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+	if (quadVAO == 0)
+	{
+		float quadVertices[] = {
+			// positions        // texture Coords
+			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		};
+		// setup plane VAO
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	}
+	glBindVertexArray(quadVAO);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glBindVertexArray(0);
+}
+// REFERENCES:
+/*
+https://learnopengl.com/Getting-started/Textures 
+https://subscription.packtpub.com/book/game-development/9781849695046/1/ch01lvl1sec12/doing-a-ripple-mesh-deformer-using-the-vertex-shader
+https://www.gamasutra.com/view/feature/2811/inexpensive_underwater_caustics_.php?print=1
+https://learnopengl.com/Advanced-Lighting/Shadows/Shadow-Mapping 
+*/
--- default.vert
+++ default.vert
@@ -20,6 +20,8 @@ uniform float time;
 //shader constants
 const float amplitude = 0.06125;
 const float frequency = 4;
+//const float amplitude = 0.2;
+//const float frequency = 2;
 const float PI = 3.14159;
 
 float PHI = 1.61803398874989484820459;  // = Golden Ratio   
@@ -34,6 +36,18 @@ void main()
 	float distance = length(aPos);  
 	//create a sin function using the distance, multiply frequency and add the elapsed time
 	float y = amplitude/1.5*sin(-PI*distance*frequency+time*2)*sin(-PI*aPos.x*frequency*50+time*2)*sin(-PI*aPos.x*frequency+time*3)*sin(-PI*aPos.z*frequency/2+time*4)*gold_noise(vec2(10.f), 12) + amplitude*sin(-PI*aPos.x*aPos.z*frequency/8+time*2) + amplitude*sin(-PI*aPos.x*frequency/16+time*2) + amplitude/10*sin(-PI*distance*frequency*5+time*2)*gold_noise(vec2(10.f), 100);	
+	//float y= amplitude*sin(-PI*distance*frequency+time);
+	//if(distance>0.5f){
+	
+//	float y = amplitude*sin(-PI*distance*frequency+time);
+//	if(distance <0.5f){
+//		y = amplitude/1.5*sin(-PI*distance*frequency+time*2)*sin(-PI*aPos.x*frequency*50+time*2)*sin(-PI*aPos.x*frequency+time*3)*sin(-PI*aPos.z*frequency/2+time*4)*gold_noise(vec2(10.f), 12) + amplitude*sin(-PI*aPos.x*aPos.z*frequency/8+time*2) + amplitude*sin(-PI*aPos.x*frequency/16+time*2) + amplitude/10*sin(-PI*distance*frequency*5+time*2)*gold_noise(vec2(10.f), 100);
+//	}
+//	}
+//	else{/
+//	y = 0.0f;
+//	}
+	//float y = amplitude*sin((sqrt(aPos.x * aPos.x + aPos.z * aPos.z)/1.0f + time) * -2*PI);
 	// Outputs the positions/coordinates of all vertices
 	gl_Position = camMatrix * vec4(aPos.x, y, aPos.z, 1.0);
 	// Assigns the colors from the Vertex Data to "color"
